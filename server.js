79c6f56e87ea0695a8a8c7ab4f91b20e5682209a
Matthew Beasley <matthewbeasley@Matthews-MacBook-Pro.local>
2022-11-05 22:32:45 -0700

    first commit of working example chat with sockets

---
+++ server.js
@@ -0,0 +1,30 @@
+import express from 'express';
+const app = express();
+import http from 'http';
+const server = http.createServer(app);
+import { Server } from "socket.io";
+const io = new Server(server);
+import path from 'path';
+import {fileURLToPath} from 'url';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+import cors from 'cors';
+
+
+app.get('/', (req, res, next) => {
+  try {
+    res.sendFile(path.join(__dirname, 'index.html'));
+  } catch (error) {
+    next(error);
+  }
+});
+
+io.on('connection', (socket) => {
+  socket.on('chat message', (msg) => {
+    io.emit('chat message', msg);
+  });
+});
+
+server.listen(3000, () => {
+  console.log('listening on *:3000');
+});
